178f3a65cf73bcb709b14aab8d352a2de6485c31
Fakeshark224 <sharkfake8@gmail.com>
2026-06-23 11:55:50 +0530

    Google adsense

--- script.js
+++ script.js
@@ -268,7 +268,7 @@
                  style="display:block"
                  data-ad-format="fluid"
                  data-ad-layout-key="-fb+5w+4e-db+86"
-                 data-ad-client="ca-pub-XXXXXXXXXXXXXXXX"
+                 data-ad-client="ca-pub-6732536198242513"
                  data-ad-slot="9876543210"></ins>
           `;
           gridElement.appendChild(adContainer);
